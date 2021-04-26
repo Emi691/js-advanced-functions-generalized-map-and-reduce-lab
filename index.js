54586b8f1d92a6b18878b951ae16b5dc89d1e27a
Emiko Nagasawa-Pijoan <enpijoan@gmail.com>
2021-04-26 00:01:19 -0700

    commit tree 142069ba4e3bafbe6aebf9d56286f372d5cb2a32

--- index.js
+++ index.js
@@ -1,1 +1,19 @@
 // Add your functions here
+function map(sourceArray, callback){
+    let newArray = []
+    sourceArray.forEach(element => newArray.push(callback(element)))
+    return newArray
+}
+
+function reduce(sourceArray, callback, startValue){
+    let total 
+    if (startValue) {
+        total = startValue
+    } else if (callback(startValue) === false) {
+        total = true 
+    } else {
+        total = 0
+    }
+    sourceArray.forEach(element => total = callback(element, total ))
+    return total
+}
